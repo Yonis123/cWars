82df3ccccc2eef46b5a9b42b163dcc3a928a4547
Yonis Nur <ynur472@gmail.com>
2023-06-16 08:53:12 -0400

    first code challenge

---
+++ js/codeWar1.js
@@ -0,0 +1,26 @@
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+function printerError(str){
+    let count = 0
+    let lettersAllowed = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    str.split('').forEach((letter) => {
+        for(let i = 0; i <= lettersAllowed.length; i++){
+            if(letter === lettersAllowed[i]){
+                count += 1
+            }
+        }
+    })
+    return(`${count}/${str.split('').length}`)
+}
+
+
+printerError('booa')
